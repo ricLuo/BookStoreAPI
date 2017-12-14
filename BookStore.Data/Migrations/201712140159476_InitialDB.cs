8b9a7a09b02e60f28196c5aeaaf5802d7cd30c76
Abhilash Reddy <abhilash.reddy@outlook.com>
2017-12-13 21:02:27 -0500

    Created Initial Roles and SuperUser

---
+++ BookStore.Data/Migrations/201712140159476_InitialDB.cs
@@ -0,0 +1,175 @@
+namespace BookStore.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 100),
+                        LastName = c.String(nullable: false, maxLength: 100),
+                        CreatedDate = c.DateTime(),
+                        CreatedBy = c.String(maxLength: 256),
+                        UpdatedDate = c.DateTime(),
+                        UpdatedBy = c.String(maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false, maxLength: 50),
+                        Description = c.String(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Copies = c.Int(nullable: false),
+                        Edition = c.String(),
+                        Isbn = c.String(),
+                        Url = c.String(),
+                        AuthorId = c.Int(nullable: false),
+                        PublishedDate = c.DateTime(nullable: false),
+                        CreatedDate = c.DateTime(),
+                        CreatedBy = c.String(maxLength: 256),
+                        UpdatedDate = c.DateTime(),
+                        UpdatedBy = c.String(maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Authors", t => t.AuthorId, cascadeDelete: true)
+                .Index(t => t.AuthorId);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                        CreatedDate = c.DateTime(),
+                        CreatedBy = c.String(maxLength: 256),
+                        UpdatedDate = c.DateTime(),
+                        UpdatedBy = c.String(maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.UserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.Roles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        FirstName = c.String(nullable: false, maxLength: 100),
+                        LastName = c.String(nullable: false, maxLength: 100),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.UserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.UserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.CategoryBooks",
+                c => new
+                    {
+                        Category_Id = c.Int(nullable: false),
+                        Book_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Category_Id, t.Book_Id })
+                .ForeignKey("dbo.Categories", t => t.Category_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Books", t => t.Book_Id, cascadeDelete: true)
+                .Index(t => t.Category_Id)
+                .Index(t => t.Book_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.UserRoles", "UserId", "dbo.Users");
+            DropForeignKey("dbo.UserLogins", "UserId", "dbo.Users");
+            DropForeignKey("dbo.UserClaims", "UserId", "dbo.Users");
+            DropForeignKey("dbo.UserRoles", "RoleId", "dbo.Roles");
+            DropForeignKey("dbo.CategoryBooks", "Book_Id", "dbo.Books");
+            DropForeignKey("dbo.CategoryBooks", "Category_Id", "dbo.Categories");
+            DropForeignKey("dbo.Books", "AuthorId", "dbo.Authors");
+            DropIndex("dbo.CategoryBooks", new[] { "Book_Id" });
+            DropIndex("dbo.CategoryBooks", new[] { "Category_Id" });
+            DropIndex("dbo.UserLogins", new[] { "UserId" });
+            DropIndex("dbo.UserClaims", new[] { "UserId" });
+            DropIndex("dbo.Users", "UserNameIndex");
+            DropIndex("dbo.UserRoles", new[] { "RoleId" });
+            DropIndex("dbo.UserRoles", new[] { "UserId" });
+            DropIndex("dbo.Roles", "RoleNameIndex");
+            DropIndex("dbo.Books", new[] { "AuthorId" });
+            DropTable("dbo.CategoryBooks");
+            DropTable("dbo.UserLogins");
+            DropTable("dbo.UserClaims");
+            DropTable("dbo.Users");
+            DropTable("dbo.UserRoles");
+            DropTable("dbo.Roles");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Books");
+            DropTable("dbo.Authors");
+        }
+    }
+}
---
+++ BookStore.Data/Migrations/Configuration.cs
@@ -0,0 +1,51 @@
+using BookStore.Data.Common;
+using BookStore.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BookStore.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BookStore.Data.Common.BookStoreDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        //protected override void Seed(BookStoreDbContext context)
+        //{
+        //    //  This method will be called after migrating to the latest version.
+
+        //    var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new BookStoreDbContext()));
+
+        //    var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new BookStoreDbContext()));
+
+        //    var user = new ApplicationUser()
+        //    {
+        //        UserName = "SuperPowerUser",
+        //        Email = "abreddy.g@gmail.com",
+        //        EmailConfirmed = true,
+        //        FirstName = "Abhi",
+        //        LastName = "Reddy",
+        //    };
+
+        //    manager.Create(user, "MySuperP@ss!");
+
+        //    if (!roleManager.Roles.Any())
+        //    {
+        //        roleManager.Create(new IdentityRole { Name = "SuperAdmin" });
+        //        roleManager.Create(new IdentityRole { Name = "Admin" });
+        //        roleManager.Create(new IdentityRole { Name = "User" });
+        //    }
+
+        //    var adminUser = manager.FindByName("SuperPowerUser");
+
+        //    manager.AddToRoles(adminUser.Id, new string[] { "SuperAdmin", "Admin" });
+        //}
+    }
+}
