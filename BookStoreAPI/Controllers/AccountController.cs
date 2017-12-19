bae4d3f20e8cee116cb1fb4b00b22b75a8b97a6b
Abhilash Reddy <abhilash.reddy@outlook.com>
2017-12-19 13:35:39 -0500

    Create User for Book Store

---
+++ BookStoreAPI/Controllers/AccountController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using BookStore.Data.Infrastructure;
+using BookStore.Data.Repositories;
+using BookStore.Models;
+using BookStoreAPI.Filters;
+using BookStoreAPI.Models;
+
+namespace BookStoreAPI.Controllers
+{
+    [RoutePrefix("api/accounts")]
+    public class AccountController : ApiController
+    {
+        private ApplicationUserManager _applicationUserManager;
+        private ApplicationRoleManager _applicationRoleManager;
+        private readonly IUserRepository _userRepository;
+
+        public AccountController(ApplicationUserManager appUserManager, ApplicationRoleManager appRoleManager,
+            IUserRepository userRepository)
+        {
+            _applicationUserManager = appUserManager;
+            _applicationRoleManager = appRoleManager;
+            _userRepository = userRepository;
+        }
+
+        //  [Authorize(Roles = "Admin")]
+        [JwtAuthentication]
+        [Route("create")]
+        [HttpPost]
+        public async Task<IHttpActionResult> CreateUser(CreateUserBindingModel createUserModel)
+        {
+            if (createUserModel != null && !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new ApplicationUser()
+            {
+                UserName = createUserModel.Username,
+                Email = createUserModel.Email,
+                FirstName = createUserModel.FirstName,
+                LastName = createUserModel.LastName,
+                DateOfBirth = createUserModel.DateOfBirth.Value
+            };
+
+            var addUserResult = await _userRepository.CreatUserAsync(user, createUserModel.Password);
+            if (!addUserResult.Succeeded)
+            {
+                return InternalServerError();
+            }
+
+            var model = new UserModel()
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                DateOfBirth = user.DateOfBirth,
+                UserName = user.UserName
+            };
+
+            Uri locationHeader = new Uri(Url.Link("GetUserById", new {id = user.Id}));
+
+            //string code = await this._userRepository.GenerateEmailConfirmationTokenAsync(user.Id);
+            return Created(locationHeader, model);
+        }
+
+        [Route("user/{id:guid}", Name = "GetUserById")]
+        public async Task<IHttpActionResult> GetUser(string id)
+        {
+            //Only SuperAdmin or Admin can delete users (Later when implement roles)
+            var user = await _userRepository.FindByUserIdAsync(id);
+
+            if (user != null)
+            {
+                var model = new UserModel()
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    DateOfBirth = user.DateOfBirth,
+                    UserName = user.UserName
+                };
+
+                return Ok(model);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ BookStoreAPI/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreAPI.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
